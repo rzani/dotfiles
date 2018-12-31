674c3834f047505d951efa1627178d1686265e0d
Rodrigo Zani <rodrigo.zhs@gmail.com>
2018-12-31 16:06:46 -0200

    adding aliases

--- alias.sh
+++ alias.sh
@@ -18,13 +18,22 @@ alias dps="docker ps --format 'table{{.ID}}\t{{.Names}}\t{{.Ports}}'"
 alias dpsa="docker ps -a"
 alias dimg="docker images"
 alias dexec="docker exec -it"
+alias dn="docker network"
+alias dc="docker container"
+alias dl="docker logs"
 
-# LARADOCK
-alias work-up="cd ~/Workspace/webserver/laradock && docker-compose up -d nginx mysql redis"
-alias work-start="cd ~/Workspace/webserver/laradock && docker-compose start nginx mysql redis"
-alias work-stop="cd ~/Workspace/webserver/laradock && docker-compose stop nginx mysql redis"
-alias work-restart="cd ~/Workspace/webserver/laradock && docker-compose restart nginx mysql redis"
-alias work-bash="cd ~/Workspace/webserver/laradock && docker-compose exec --user=laradock workspace bash"
+# GO
+alias go-coverage="go test -coverprofile=coverage.out && go tool cover -html=coverage.out && rm -rf coverage.out"
+
+# Python
+alias p3="python3"
+
+# LARADOCK - DEV
+alias dev-up="cd ~/Workspace/webserver/dev && docker-compose up -d nginx mariadb redis && cd -"
+alias dev-start="cd ~/Workspace/webserver/dev && docker-compose up -d nginx mariadb redis && cd -"
+alias dev-stop="cd ~/Workspace/webserver/dev && docker-compose stop && cd -"
+alias dev-restart="cd ~/Workspace/webserver/dev && docker-compose restart && cd -"
+alias dev-bash="cd ~/Workspace/webserver/dev && docker-compose exec --user=laradock workspace bash && cd -"
 
 # FILES
 alias hosts="sudo vim /etc/hosts"
@@ -35,3 +44,8 @@ alias t="tmux"
 alias ta="tmux a"
 alias tls="tmux ls"
 alias tn="tmux new -s "
+
+# SERVERS
+alias ns2="ssh ns2 -t byobu"
+alias ns1="ssh ns1 -t byobu"
+
